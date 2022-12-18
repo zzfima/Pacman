59d61e4acee1b2bf0eb19c68d2f4ef5e82ef417f
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-12-18 06:10:01 +0200

    read input

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.19
 
 require github.com/stretchr/testify v1.8.1
 
+require github.com/mattn/go-runewidth v0.0.9 // indirect
+
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/nsf/termbox-go v1.1.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	term "github.com/nsf/termbox-go"
 )
 
 // LoadMaze read maze into string array
@@ -24,6 +26,35 @@ func LoadMaze(path string) ([]string, error) {
 	return mz, nil
 }
 
+// ReadInput read input includes esc, arrows etc
+func ReadInput() (string, error) {
+	e := term.Init()
+	defer term.Close()
+	if e != nil {
+		return "", e
+	}
+	ev := term.PollEvent()
+
+	if ev.Ch != 0 {
+		return string(ev.Ch), nil
+	}
+
+	switch ev.Key {
+	case term.KeyEsc:
+		return "ESC", nil
+	case term.KeyArrowUp:
+		return "UP", nil
+	case term.KeyArrowDown:
+		return "DOWN", nil
+	case term.KeyArrowLeft:
+		return "LEFT", nil
+	case term.KeyArrowRight:
+		return "RIGHT", nil
+	}
+
+	return "", nil
+}
+
 // PrintMaze printing maze to cmd
 func PrintMaze(maze []string) {
 	for _, line := range maze {
@@ -40,7 +71,8 @@ func main() {
 
 	for {
 		PrintMaze(mazeMap)
-
+		input, e := ReadInput()
+		fmt.Println(input, e)
 		break
 	}
 }
